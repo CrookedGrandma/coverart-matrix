fe5cd374e99d2552f850db1639ae90276d05307a
CrookedGrandma <k.t.noot@live.nl>
2022-01-31 01:05:04 +0100

    Finally be able to show an image

--- coverart.py
+++ coverart.py
@@ -1,27 +1,28 @@
+from PIL import Image
 from samplebase import SampleBase
-from random import randint
-import math
+import numpy as np
+import sys
 
 class TestBoy(SampleBase):
     def __init__(self, *args, **kwargs):
         super(TestBoy, self).__init__(*args, **kwargs)
-    
-    def col(self, val, mult):
-        w = self.matrix.width
-        f = val / w
-        f2pi = 2 * math.pi * f
-        return math.floor((math.sin(mult * f2pi) + 1) / 2 * 255)
+        self.parser.add_argument("-i", "--img", help="The image to show", default="./testimg2.png")
     
     def run(self):
+        img = Image.open(self.args.img)
+        img.thumbnail((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+        img = img.convert("RGB")
+        px = np.array(img)
         offset_canvas = self.matrix.CreateFrameCanvas()
         while True:
             for x in range(0, self.matrix.width):
                 for y in range(0, self.matrix.height):
-                    offset_canvas.SetPixel(x, y, self.col(x+y, 1), self.col(x+y, 1.4), self.col(x+y, 1.8))
+                    offset_canvas.SetPixel(x, y, px[x, y, 0], px[x, y, 1], px[x, y, 2])
             offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
 
 
 if __name__ == "__main__":
+    
     testboy = TestBoy()
     if (not testboy.process()):
         testboy.print_help()
