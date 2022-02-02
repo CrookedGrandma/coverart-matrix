33aaf079cd5d22218d7aa50a39542a5fe61ce44f
CrookedGrandma <k.t.noot@live.nl>
2022-02-02 11:18:09 +0100

    WIP: Authorize Spotify on frontend

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
-import sys
+import requests
+import spotipy
 import time
 import threading
 from configparser import ConfigParser
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect
+from urllib.parse import urlencode
 
+load_dotenv()
 app = Flask(__name__)
 
 cfgfile = "config.ini"
@@ -14,6 +18,22 @@ d = "DEFAULT"
 dcfg = cfg[d]
 
 rgb_thread = None
+
+
+def test_sp_connection(sp):
+    try:
+        sp.me()
+    except spotipy.exceptions.SpotifyException:
+        cfg.set(d, "token", "")
+        with open(cfgfile, "w") as configfile:
+            cfg.write(configfile)
+
+
+scope = "user-read-currently-playing"
+sp = None
+if dcfg["token"] != "":
+    sp = spotipy.Spotify(auth=dcfg["token"])
+    test_sp_connection(sp)
 
 
 def _start_rgb(brightness):
@@ -42,10 +62,32 @@ def stop_rgb():
 def main_page():
     power = dcfg["power"]
     brightness = int(dcfg["brightness"])
-    return render_template("index.html", power=power, brightness=brightness)
+    token = dcfg["token"]
+    return render_template("index.html", power=power, brightness=brightness, token=token)
 
 
-@app.route('/power', methods=["GET", "POST"])
+@app.route('/login', methods=["POST"])
+def handle_login():
+    getparams = urlencode(dict(
+        response_type="code",
+        client_id=os.getenv("SPOTIPY_CLIENT_ID"),
+        scope=scope,
+        redirect_uri=os.getenv("SPOTIPY_REDIRECT_URI"))
+    )
+    return redirect(f"https://accounts.spotify.com/authorize?{getparams}")
+
+
+@app.route('/logincallback')
+def handle_login_done():
+    code = request.args.get("code")
+    # POST request to 'https://accounts.spotify.com/api/token'
+    global sp
+    sp = spotipy.Spotify(auth=code)
+    test_sp_connection(sp)
+    return redirect("/")
+
+
+@app.route('/power', methods=["POST"])
 def handle_power():
     power = request.form["power"]
     brightness = int(dcfg["brightness"])
