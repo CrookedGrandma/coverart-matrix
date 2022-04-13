23e44a155dc5a2d610aba7cb6ba5840c58232828
CrookedGrandma <k.t.noot@live.nl>
2022-04-13 13:00:59 +0200

    Expand TCP test script to login to Spotipy

--- tcptest.py
+++ tcptest.py
@@ -1,4 +1,21 @@
 import socketserver
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+scope = "user-read-currently-playing"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, open_browser=False))
+
+def check_login():
+    auth = sp.auth_manager
+    token = auth.validate_token(auth.cache_handler.get_cached_token())
+    if token is not None:
+        if auth.is_token_expired(token):
+            auth.refresh_access_token(token["refresh_token"])
+        return True
+    return False
 
 class MyTCP(socketserver.BaseRequestHandler):
     def handle(self):
@@ -6,11 +23,30 @@ class MyTCP(socketserver.BaseRequestHandler):
         self.data = self.request.recv(1024).strip()
         print("{} wrote:".format(self.client_address[0]))
         print(self.data)
-        # just send back the same data, but upper-cased
-        self.request.sendall(self.data.upper())
+        # handle requests
+        if self.data.startswith(b"LOGIN"):
+            if check_login():
+                self.request.sendall(bytes(f"CACHED:{sp.me()['display_name']}", "utf-8"))
+            else:
+                url = sp.auth_manager.get_authorize_url()
+                self.request.sendall(bytes(url, "utf-8"))
+        elif self.data.startswith(b"REDIR"):
+            url = self.data.split(b":", 1)[1].decode("utf-8")
+            try:
+                code = sp.auth_manager.parse_response_code(url)
+                sp.auth_manager.get_access_token(code, as_dict=False)
+                if check_login():
+                    self.request.sendall(bytes(str(f"LOGGEDIN:{sp.me()['display_name']}"), "utf-8"))
+                else:
+                    self.request.sendall(b"login failed")
+            except spotipy.SpotifyOauthError:
+                self.request.sendall(b"login failed")
+        else:
+            self.request.sendall(b"wrong request")
 
 
 if __name__ == "__main__":
     HOST, PORT = "0.0.0.0", 9999
     server = socketserver.TCPServer((HOST, PORT), MyTCP)
+    print("Starting TCP server")
     server.serve_forever()
